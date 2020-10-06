f084b7f7e599ebc40a93b98dfe9fc7a50aac8d9d
Carlos Castro <carlos.lara@activeprime.com>
2020-10-05 22:58:43 -0500

    Initial Vue Hackaton

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data: () => ({
+    info: null
+  }),
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router'
+
+// Pages
+import Home from '@/pages/home'
+import Login from '@/pages/login'
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/login',
+      component: Login
+    }
+  ],
+  linkActiveClass: "active",
+  mode: "history"
+})
+
+new Vue({
+  vuetify,
+  router,
+  render: h => h(App)
+}).$mount('#app')
